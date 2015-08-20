c46c0a45a961320ec0c8f770eadc1457530759c0
Zee <zemskov@tillypad.ru>
2015-08-20 19:04:24 +0300

    добавления

--- external_GetBuyerXML.sql
+++ external_GetBuyerXML.sql
@@ -4,6 +4,7 @@ GO
 
 CREATE PROCEDURE dbo.external_GetBuyerXML  (
     @part_ID UNIQUEIDENTIFIER,
+	@addr_ID UNIQUEIDENTIFIER,
 	@BuyerXML XML OUTPUT)
 AS
 /*
@@ -57,6 +58,8 @@ JOIN tp_StoreRequests           R ON R.strqt_ID = M.doc_ID
 --JOIN tp_StoreRequestItems       I ON I.strqti_strqt_ID = M.doc_ID
 WHERE M.messageId = @messageId*/
 
+
+
 SET @BuyerXML = 
 (
 	SELECT 
@@ -65,14 +68,18 @@ SET @BuyerXML =
 		,dbo.f_MultiLanguageStringToStringByLanguage1(firm_INN, 25) N'organization/inn' --ИНН поставщика для ЮЛ
 		,dbo.f_MultiLanguageStringToStringByLanguage1(firm_KPP, 25) N'organization/kpp' --КПП поставщика только для ЮЛ
 		--российский адрес
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_RegionCode, 25) N'russianAddress/regionISOCode'
-    	,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Area, 25) N'russianAddress/district'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_City, 25) N'russianAddress/city'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Village, 25) N'russianAddress/settlement'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Street, 25) N'russianAddress/street'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_House, 25) N'russianAddress/house'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Apartment, 25) N'russianAddress/flat'
-        ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_PostCode, 25) N'russianAddress/postalCode'
+		,(SELECT
+            dbo.f_MultiLanguageStringToStringByLanguage1(addr_RegionCode, 25) N'regionISOCode'
+    	    ,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Area, 25) N'district'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_City, 25) N'city'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Village, 25) N'settlement'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Street, 25) N'street'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_House, 25) N'house'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_Apartment, 25) N'flat'
+			,dbo.f_MultiLanguageStringToStringByLanguage1(addr_PostCode, 25) N'postalCode'
+		FROM tp_Addresses
+		WHERE addr_ID = @addr_ID
+		FOR XML PATH(N'russianAddress'), TYPE) AS [*]
 		-- Контактная информация
         ,dbo.f_MultiLanguageStringToStringByLanguage1(ISNULL(PD.pepl_PhoneWork, ''), 25) N'contactlInfo/CEO/orderContact'--телефон контактного лица
         ,dbo.f_MultiLanguageStringToStringByLanguage1(ISNULL(PD.pepl_PhoneCell, ''), 25) N'contactlInfo/CEO/fax'--факс контактного лица
@@ -81,7 +88,7 @@ SET @BuyerXML =
 		
 	FROM tp_Partners       P
 	LEFT JOIN tp_Firms     F ON F.firm_ID = P.part_firm_ID
-	LEFT JOIN tp_Addresses A ON A.addr_obj_ID = F.firm_ID AND addr_Type = 2 -- CASE WHEN T2.part_firm_ID IS NULL THEN 3 ELSE 2 END AdddrType  -- рабочий (ФЛ) или юридический (ЮЛ) адрес
+	--LEFT JOIN tp_Addresses A ON A.addr_obj_ID = F.firm_ID AND addr_Type = 2 -- CASE WHEN T2.part_firm_ID IS NULL THEN 3 ELSE 2 END AdddrType  -- рабочий (ФЛ) или юридический (ЮЛ) адрес
 	LEFT JOIN tp_People PD ON PD.pepl_ID = F.firm_pepl_ID_Director
 	WHERE part_ID = @part_ID
 	FOR XML PATH(N'buyer'), TYPE
